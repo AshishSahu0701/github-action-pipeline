9fb8ef83cab51047d6296ed4b151efe1f9706b36
Ashish Sahu <sahu.ashish0701@gmail.com>
2026-08-11 23:43:35 +0530

    initial code3

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "4.80.0"
+      source  = "hashicorp/azurerm"
+      version = "4.80.0"
     }
   }
 
   backend "azurerm" {
-    resource_group_name = "rg-tfstate"
+    resource_group_name  = "rg-tfstate"
     storage_account_name = "adoashishtfstate"
-    container_name = "adoashishtfstatecontainer"
-    key = "/github_action/demo/pipeline.tfstate"
+    container_name       = "adoashishtfstatecontainer"
+    key                  = "/github_action/demo/pipeline.tfstate"
   }
 }
 
 provider "azurerm" {
   features {
-    
+
   }
 }
 
 resource "azurerm_resource_group" "RG" {
-  name = "rg-githubaction"
+  name     = "rg-githubaction"
   location = "central india"
 }
